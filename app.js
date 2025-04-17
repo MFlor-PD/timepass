81082dd177a2d7656f12c36eb67d841bf4376215
Flor <mariaflorenciaperezd@gmail.com>
2025-04-17 18:48:53 +0200

    app.js set con EXPRESS, REQUIRES, RUTAS, LISTEN PORT

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const INDEX = require('./routes/index');
+const ENDROUTE = require('./routes/endpoint');
+
+const horaMiddleware = ('./middleware/horaMiddleware'); 
+const validarHora = require('./middleware/validarHora');
+
+app.use('/', indexRouter);
+app.use('/endroute', endrouteRouter);
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
