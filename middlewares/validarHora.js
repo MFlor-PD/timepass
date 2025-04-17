20b77e74989666f65c75a7897d7b0a9854811340
Flor <mariaflorenciaperezd@gmail.com>
2025-04-17 20:26:23 +0200

    configuracion de la validacion de hora

--- middlewares/validarHora.js
+++ middlewares/validarHora.js
@@ -1,1 +1,13 @@
-//`validarHora.js`: Aquí estará la lógica de si accedemos a la siguiente ruta
+const validarHora = (req, res, next) => {
+    const horaActual = new Date().getHours();
+    const horaLimite1 = 12;
+    const horaLimite2 = 24;
+
+
+    if (horaActual >= horaLimite1 && horaActual <= horaLimite2) {
+        next();
+    } else {
+        res.status(400).send('Aún no son las 12:00 de la maniana, espera para entrar'); 
+    }
+}
+module.exports = validarHora;
