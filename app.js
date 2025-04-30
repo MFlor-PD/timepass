55e46246a68c12f661e4aed9d618dff7cd60932d
Flor <mariaflorenciaperezd@gmail.com>
2025-04-30 11:26:55 +0200

    mensaje

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 const indexRouter = require('./routes/index');
 const endrouteRouter = require('./routes/endroute');
@@ -12,8 +13,8 @@ app.use('/', indexRouter);
 app.use('/endroute', validarHora, endrouteRouter);
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ router.get('/', (req, res) => {
   res.send(`
     <h1>BIENVENIDO</h1>
     <p>La hora actual es: ${req.hora}</p>
+    <p>${req.query.mensaje || ''}</p>
     <a href="/endroute"><button>ENTRAR</button></a>`);
 })
 
